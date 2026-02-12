59bcd9facd0e72efd0c56113121c6003e35aa3e2
aion-shahriar <aionshahriar23@gmail.com>
2026-02-12 13:44:07 +0600

    Banner done

--- src/pages/HomePage/Banner.jsx
+++ src/pages/HomePage/Banner.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import bannerImg1 from '../../assets/__b1.png';
+import bannerImg2 from '../../assets/__b2.png';
+import bannerImg3 from '../../assets/__b3.png';
 
-export default function Banner() {
-  return (
-    <div>Banner</div>
-  )
-}
+const Banner = () => {
+    return (
+        
+            
+            <Carousel autoPlay={true} infiniteLoop={true} className=''>
+                <div>
+                    <img src={bannerImg1} alt="Banner 1" className='' />
+                    {/* <p className="legend">Legend 1</p> */}
+                </div>
+                <div>
+                    <img src={bannerImg2} alt="Banner 2" className='' />
+                    {/* <p className="legend">Legend 2</p> */}
+                </div>   
+                <div>
+                    <img src={bannerImg3} alt="Banner 3" className='' />
+                    {/* <p className="legend">Legend 3</p> */}
+                </div>
+            </Carousel>
+        
+    );
+};
+
+export default Banner;
